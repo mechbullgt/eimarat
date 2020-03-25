521f46abf6f2a16e03624b6641137971c002238b
xyz.maqbool@gmail.com <xyz.maqbool@gmail.com>
2020-03-25 22:24:42 +0530

    User is able to add data to the sheet: Success

---
+++ lib/controller/form_controller.dart
@@ -0,0 +1,33 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import '../model/form.dart';
+
+/// FormController is a class which does work of saving FeedbackForm in Google Sheets using 
+/// HTTP GET request on Google App Script Web URL and parses response and sends result callback.
+class FormController {
+  // Callback function to give response of status of current request.
+  final void Function(String) callback;
+
+  // Google App Script Web URL.
+  static const String URL = "https://script.google.com/macros/s/AKfycby1hqWxA1nlELW6BoVLQ1dlBt5ithuI4I2ZwRcT91npSVty-ZlL/exec";
+  
+  // Success Status Message
+  static const STATUS_SUCCESS = "SUCCESS";
+
+  // Default Contructor
+  FormController(this.callback);
+
+  /// Async function which saves feedback, parses [feedbackForm] parameters
+  /// and sends HTTP GET request on [URL]. On successful response, [callback] is called.
+  void submitForm(FeedbackForm feedbackForm) async {
+    try {
+      await http.get(
+        URL + feedbackForm.toParams()
+      ).then((response){
+        callback(convert.jsonDecode(response.body)['status']);
+      });    
+    } catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
+import 'controller/form_controller.dart';
+import 'model/form.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Google Sheet Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Google Sheet Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +27,142 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  // TextField Controllers
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController mobileNoController = TextEditingController();
+  TextEditingController feedbackController = TextEditingController();
+
+  // Method to Submit Feedback and save it in Google Sheets
+  void _submitForm() {
+    // Validate returns true if the form is valid, or false
+    // otherwise.
+    if (_formKey.currentState.validate()) {
+      // If the form is valid, proceed.
+      FeedbackForm feedbackForm = FeedbackForm(
+        nameController.text, 
+        emailController.text, 
+        mobileNoController.text, 
+        feedbackController.text
+      );
+
+      FormController formController = FormController((String response) {
+          print("Response: $response");
+          if (response == FormController.STATUS_SUCCESS) {
+            // Feedback is saved succesfully in Google Sheets.
+            _showSnackbar("Feedback Submitted");
+          } else {
+            // Error Occurred while saving data in Google Sheets.
+            _showSnackbar("Error Occurred!");
+          }
+        }
+      );
+     
+      _showSnackbar("Submitting Feedback");
+
+      // Submit 'feedbackForm' and save it in Google Sheets.
+      formController.submitForm(feedbackForm);
+    }
+  }
+
+  // Method to show snackbar with 'message'.
+  _showSnackbar(String message) {
+      final snackBar = SnackBar(content: Text(message));
+      _scaffoldKey.currentState.showSnackBar(snackBar); 
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      key: _scaffoldKey,  
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child:
+                  Padding(padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: nameController,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter Valid Name';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Name'
+                        ),
+                      ),
+                      TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (!value.contains("@")) {
+                            return 'Enter Valid Email';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email'
+                        ),
+                      ),
+                      TextFormField(
+                        controller: mobileNoController,
+                        validator: (value) {
+                          if (value.trim().length != 10) {
+                            return 'Enter 10 Digit Mobile Number';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Mobile Number',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: feedbackController,
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Enter Valid Feedback';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.multiline,
+                        decoration: InputDecoration(
+                          labelText: 'Feedback'
+                        ),
+                      ),
+                    ],
+                  ),
+                ) 
+              ),
+              RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed:_submitForm,
+                child: Text('Submit Feedback'),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
